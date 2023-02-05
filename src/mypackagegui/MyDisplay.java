c7e826afb464cec722c9d9b50d13ecaec3b82af6
BenBen <vince7ben23@gmail.com>
2023-02-05 21:53:17 +0800

    init project

---
+++ src/mypackagegui/MyDisplay.java
@@ -0,0 +1,26 @@
+package mypackagegui;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet {
+	public void setup() {
+		size(600, 600);
+		background(230);
+		
+	}
+	
+	public void draw() {
+		fill(255, 255, 0);
+		ellipse(300, 300, 200, 200);
+		
+		fill(0, 0, 0);
+		ellipse(250, 275, 25, 50);
+		ellipse(350, 275, 25, 50);
+		arc(300, 350, 120, 50, 0, PI);
+		
+		fill(100, 100, 100);
+		ellipse(300, 475, 200, 50);
+		stroke(100, 100, 100);
+		
+	}
+}
